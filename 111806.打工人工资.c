7a63d49d9905085e5f1f8e8eb3a67d47a9a5d78d
software203hj <74293590+software203hj@users.noreply.github.com>
2020-11-18 10:16:26 +0800

    Create 111806.打工人工资.c

---
+++ 111806.打工人工资.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(void)
+{
+			int n;
+	printf("打工人工资等级划分\n");
+		printf("请输入你的工资\n");
+		scanf("%d",&n);
+			if(n<-1)
+				printf("你还有花呗没还，无法使用该程序。\n");
+			else if(n>=0 && n<=500)
+				printf("赤贫\n");
+			else if(n>=501 && n<=1500)
+				printf("贫困\n");
+			else if(n>=1501 && n<=3000)
+				printf("普通打工人\n");
+			else if(n>=3001 && n<=5000)
+				printf("普通清洁工\n");
+			else if(n>=5001 && n<=12000)
+				printf("水电工工资\n");
+			else if(n>=12000 && n<=50000)
+				printf("脑袋大脖子粗不是老板就是伙夫。\n");
+			else if(n>=50000 && n<=200000000)
+				printf("程序员\n");
+			return 0;
+
+
+}

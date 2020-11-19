f184a5caff1b17ca0d08bc873bd90ac67a81c5af
software203hj <74293590+software203hj@users.noreply.github.com>
2020-11-19 19:45:05 +0800

    Create 111910.性别.c

---
+++ 111910.性别.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ void main()
+ {
+	 int x;
+		 printf("请输入“0”或“1”！\n");
+		 scanf("%d",&x);
+		 if(x==1)
+	printf("男\n");
+		 else if(x==0)
+			 printf("女\n");
+		 else 
+			 printf("输入有误，请重新输入！\n");
+	
+ 
+ }

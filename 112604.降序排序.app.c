e97bdead47116d780414807d28e63311a6a54171
software203hj <74293590+software203hj@users.noreply.github.com>
2020-11-26 20:06:48 +0800

    Create 112604.降序排序.app.c

---
+++ 112604.降序排序.app.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,b,c;
+	 int x;
+	   printf("请输入三个数字！\n");
+	       scanf("%d%d%d",&a,&b,&c);
+	if(a<b)
+	{
+	x=a;
+	a=b;
+	b=x;
+	}
+	if(a<c)
+	{
+		x=a;
+	a=c;
+	c=x;
+	}
+	if(b<c)
+	{
+	x=b;
+	b=c;
+	c=x;
+	
+	}
+	printf("%d%d%d\n",a,b,c);
+	return 0;
+}
+	
+

ae12a99dba920979ef85054a603205ff969dcd36
software203hj <74293590+software203hj@users.noreply.github.com>
2020-12-09 17:05:38 +0800

    Create 120905.串长度.app.c

---
+++ 120905.串长度.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a1[]="hello,world!";
+		char a2[]="你好呀！";
+		printf("%d",sizeof(a1));
+		printf("\n");
+		printf("%d",sizeof(a2));
+		printf("\n");
+		
+
+return 0;
+}

a31eb7de505e29d0d9ed2160dd9a580a3bc16b04
software203hj <74293590+software203hj@users.noreply.github.com>
2020-12-09 08:41:30 +0800

    Update 120303.数组清零.app.c

--- 120303.数组清零.app.c
+++ 120303.数组清零.app.c
@@ -1,3 +1,8 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main(void)
+{
 int x;
 		int y;
 		int group[20][10];
@@ -11,6 +16,8 @@ int x;
 		}
 		printf("\n");
 		}
+			printf("数据正在清零，请稍后5秒！\n");
+		Sleep(5000);
 		for(x=0;x<20;x++)
 		{
 		for(y=0;y<10;y++)
@@ -23,3 +30,4 @@ int x;
 		}
 	return 0;
 }
+

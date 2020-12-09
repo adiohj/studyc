57891396b18767e29f86ea89b26f3b8d5db58de5
software203hj <74293590+software203hj@users.noreply.github.com>
2020-12-09 09:10:55 +0800

    Create 120901.数组元素递减.app.c

---
+++ 120901.数组元素递减.app.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(void)
+{
+int x;
+		int y;
+		int i=90;
+		int group[9][10];
+		for(x=0;x<9;x++)
+		{
+		for(y=0;y<10;y++)
+		{
+			group[x][y]=0;
+		printf("%d ",group[x][y]);
+
+		}
+		printf("\n");
+		}
+	
+		for(x=0;x<9;x++)
+		{
+		for(y=0;y<10;y++)
+		{
+			group[x][y]=i--;
+		printf("%d ",group[x][y]);
+
+		}
+		printf("\n");
+		}
+		
+	return 0;
+}
